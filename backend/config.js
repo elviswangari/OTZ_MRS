70e0c21fe2140c41158a89da1cd4b34ec4312316
elvis wangari <elviswangare@gmail.com>
2024-01-26 10:28:28 +0300

    deleted source code to new branch

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  PORT: process.env.PORT || 3001,
-  DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/otzModule',
-  SECRET_KEY: process.env.SECRET_KEY || 'default_secret_key',
-};
